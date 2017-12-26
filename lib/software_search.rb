757202904cc6c171ff3fd4a95c90478c6dcc9fb2
Hoon Lee <yl.hoony@gmail.com>
2017-12-26 02:25:11 -0800

    updated execution file

--- lib/software_search.rb
+++ lib/software_search.rb
@@ -1,5 +1,13 @@
-require "software_search/version"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
-module SoftwareSearch
+require_relative './software_search/version'
+require_relative './software_search/cli.rb'
+require_relative './software_search/scraper.rb'
+require_relative './software_search/category.rb'
+require_relative './software_search/software.rb'
+
+# module SoftwareSearch
   # Your code goes here...
-end
+# end
